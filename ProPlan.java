6ccd5c4febc69c170fe82d55c2d684b82f5a1e08
KONAIN-WORSI <konainworsi@gmail.com>
2026-02-23 15:31:56 +0545

    started proplan programming

--- ProPlan.java
+++ ProPlan.java
@@ -5,6 +5,27 @@
  * @author (your name)
  * @version (a version number or a date)
  */
-public class ProPlan
+public class ProPlan extends AIModel
 {
+    private String TeamMember;
+    final static int SLOTS_FOR_PROPLAN = 4;
+    
+    ProPlan(String TeamMember, String modelName, double price, int paramaterCount, String contextWindow) {
+        super(modelName, price, paramaterCount, contextWindow);
+        this.TeamMember = TeamMember;
+    }
+    
+    public String getTeamMember() {
+        return this.TeamMember;
+    }
+    
+    public void setTeamMember(String TeamMember) {
+        this.TeamMember = TeamMember;
+    }
+    
+    public String addTeamMemeber(String teamMemeberName) {
+        
+    }
+    
+    
 }
